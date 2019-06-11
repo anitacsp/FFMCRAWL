fed8cfa8302fba2e97b957a04e0dd4da5d64db49
Chia Si Pei Anita <anita.chia.2016@sis.smu.edu.sg>
2019-06-10 22:37:01 -0400

    removal of stop words: increased accuracy by 0.09

--- NaiveBayesClassification.py
+++ NaiveBayesClassification.py
@@ -2,6 +2,8 @@ import re
 import numpy as np
 import pandas as pd
 import csv
+import nltk
+from nltk.corpus import stopwords
 
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
@@ -38,30 +40,33 @@ allSample['id'] = np.arange(len(allSample))
 allSample['nom'] = [normalize(text) for text in allSample['Title']]
 allSample.set_index('id', inplace=True)
 
+stopWords = set(stopwords.words('english'))
+allSample['nom'] = allSample['nom'].apply(lambda x: ' '.join([item for item in x.split() if item not in stopWords]))
+
+
 #transform text data to vectors
 vec = CountVectorizer()
-#allSample['x'] = vec.fit_transform(allSample['nom'])
 x =vec.fit_transform(allSample['nom'])
-#cat.to_csv("test.csv", encoding ='utf-8')
 
-#encode = LabelEncoder()
-#y = encode.fit_transform(allSample['ESG'])
-#allSample['y-axis'] = allSample['ESG']
 
 #split into train & test datasets
 x_train, x_test, y_train, y_test = train_test_split(x,allSample['ESG'], test_size = 0.2, random_state=1)
 
+#Naive Bayes model chosen. Light weight
 baes = MultinomialNB()
+#training our model
 baes.fit(x_train,y_train)
-#baes.score(x_test, y_test)
+
+#Print accuracy no. of rights/total results
 print(baes.score(x_test, y_test))
+#Prints confusion matrix: postive, false postive, negative, false negative
 print(confusion_matrix(y_test,baes.predict(x_test), labels = [0,1]))
 
+#Preparing dataset to be exported to csv
 sub = pd.DataFrame(data=y_test)
 sub['pred'] = baes.predict(x_test)
 
 output = pd.merge(allSample, sub, on = 'id')
-
 
 usableData = output[['Title','compound', 'ESG_x', 'pred']]
 usableData.to_csv('out2.csv')
