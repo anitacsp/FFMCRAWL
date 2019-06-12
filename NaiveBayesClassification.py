72c0a77292de9a353d321ede1dc67d7585cfe63e
Chia Si Pei Anita <anita.chia.2016@sis.smu.edu.sg>
2019-06-12 03:31:40 -0400

    editted to output predictions with no labels

--- NaiveBayesClassification.py
+++ NaiveBayesClassification.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import csv
 import nltk
+import time
 from nltk.corpus import stopwords
 
 from sklearn.naive_bayes import MultinomialNB
@@ -23,34 +24,62 @@ def normalize(text):
 
     return text
 
-
+start = time.time()
 #retrieve ESG samples & non-ESG samples
-esgSample = pd.read_csv(r"C:\Users\chias\source\repos\FFMCRAWL\data files\ESG Dataset.csv")
-sample = pd.read_csv(r"C:\Users\chias\source\repos\FFMCRAWL\data files\sample.csv")
+esgTrain = pd.read_csv(r"C:\Users\chias\source\repos\FFMCRAWL\data files\ESG Dataset.csv")
+noesgTrain = pd.read_csv(r"C:\Users\chias\source\repos\FFMCRAWL\data files\sample.csv")
+print("Training dataset imported")
+
+etTest = pd.read_csv(r"C:\Users\chias\source\repos\FFMCRAWL\et.csv")
+print("Testing dataset imported")
+
+
 
 #retrieve useful columns with useful data
-esgUse = esgSample[['Title', 'ESG', 'neg', 'neu', 'pos', 'compound' ]]
-samUse = sample[['Title', 'ESG', 'neg', 'neu', 'pos', 'compound']]
+esgUse = esgTrain[['Title', 'ESG', 'neg', 'neu', 'pos', 'compound' ]]
+samUse = noesgTrain[['Title', 'ESG', 'neg', 'neu', 'pos', 'compound']]
+etUse = etTest[['Title', 'neg', 'neu', 'pos', 'compound']]
 
 #join both sets of data together
 allSample = esgUse.append(samUse)
-allSample['id'] = np.arange(len(allSample))
+
 
 #normalize all title
 allSample['nom'] = [normalize(text) for text in allSample['Title']]
+allSample['id'] = np.arange(len(allSample))
 allSample.set_index('id', inplace=True)
+print("Training dataset normalized")
+
+etUse['nom'] = [normalize(text) for text in etUse['Title']]
+etUse['id'] = np.arange(len(etUse))
+etUse.set_index('id', inplace=True)
+print("Testing dataset normalized")
+
 
 stopWords = set(stopwords.words('english'))
 allSample['nom'] = allSample['nom'].apply(lambda x: ' '.join([item for item in x.split() if item not in stopWords]))
+print("Training stopwords removed")
+
+etUse['nom'] = etUse['nom'].apply(lambda x: ' '.join([item for item in x.split() if item not in stopWords]))
+print("Testing stopwords removed")
 
 
 #transform text data to vectors
 vec = CountVectorizer()
 x =vec.fit_transform(allSample['nom'])
+print("Training data vectorized")
+
+test = vec.transform(etUse['nom'])
+print("Testing data vectorized")
 
 
 #split into train & test datasets
 x_train, x_test, y_train, y_test = train_test_split(x,allSample['ESG'], test_size = 0.2, random_state=1)
+x_train = x
+y_train = allSample['ESG']
+x_test = test
+print(x_train.shape)
+print(x_test.shape)
 
 #Naive Bayes model chosen. Light weight
 baes = MultinomialNB()
@@ -58,15 +87,18 @@ baes = MultinomialNB()
 baes.fit(x_train,y_train)
 
 #Print accuracy no. of rights/total results
-print(baes.score(x_test, y_test))
+#print(baes.score(x_test, y_test))
 #Prints confusion matrix: postive, false postive, negative, false negative
-print(confusion_matrix(y_test,baes.predict(x_test), labels = [0,1]))
+#print(confusion_matrix(y_test,baes.predict(x_test), labels = [0,1]))
 
 #Preparing dataset to be exported to csv
-sub = pd.DataFrame(data=y_test)
-sub['pred'] = baes.predict(x_test)
+output = pd.DataFrame(data=etUse)
 
-output = pd.merge(allSample, sub, on = 'id')
+output['pred'] = baes.predict(x_test)
 
-usableData = output[['Title','compound', 'ESG_x', 'pred']]
-usableData.to_csv('out2.csv')
+print(list(output))
+usableData = output[[ 'Title', 'compound', 'pred']]
+usableData.to_csv('etOutput.csv')
+
+end = time.time() - start
+print("Total time taken: "+ str(end))
